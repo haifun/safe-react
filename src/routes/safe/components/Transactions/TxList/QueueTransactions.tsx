71062071e09696cbb59dec0c87cf72d652359ef6
Usame Algan <usame.algan@gnosis.pm>
2022-03-29 12:10:49 +0200

    chore: address feedback

--- src/routes/safe/components/Transactions/TxList/QueueTransactions.tsx
+++ src/routes/safe/components/Transactions/TxList/QueueTransactions.tsx
@@ -37,21 +37,19 @@ export const QueueTransactions = (): ReactElement => {
   }
 
   return (
-    <>
-      <BatchExecuteHoverProvider>
-        {batchExecute && <MemoizedBatchExecute />}
-        <TxsInfiniteScroll next={next} hasMore={hasMore} isLoading={isLoading}>
-          {/* Next list */}
-          <TxLocationContext.Provider value={{ txLocation: 'queued.next' }}>
-            {transactions.next.count !== 0 && <QueueTxList transactions={transactions.next.transactions} />}
-          </TxLocationContext.Provider>
+    <BatchExecuteHoverProvider>
+      {batchExecute && <MemoizedBatchExecute />}
+      <TxsInfiniteScroll next={next} hasMore={hasMore} isLoading={isLoading}>
+        {/* Next list */}
+        <TxLocationContext.Provider value={{ txLocation: 'queued.next' }}>
+          {transactions.next.count !== 0 && <QueueTxList transactions={transactions.next.transactions} />}
+        </TxLocationContext.Provider>
 
-          {/* Queue list */}
-          <TxLocationContext.Provider value={{ txLocation: 'queued.queued' }}>
-            {transactions.queue.count !== 0 && <QueueTxList transactions={transactions.queue.transactions} />}
-          </TxLocationContext.Provider>
-        </TxsInfiniteScroll>
-      </BatchExecuteHoverProvider>
-    </>
+        {/* Queue list */}
+        <TxLocationContext.Provider value={{ txLocation: 'queued.queued' }}>
+          {transactions.queue.count !== 0 && <QueueTxList transactions={transactions.queue.transactions} />}
+        </TxLocationContext.Provider>
+      </TxsInfiniteScroll>
+    </BatchExecuteHoverProvider>
   )
 }
